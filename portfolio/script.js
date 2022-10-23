4bda3819d5a6631937f03023772877f4ddca83fe
Rivelino Camurça Rodriguez <rivelinocamurca@hotmail.com>
2022-10-23 20:18:32 -0300

    new order

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,10 +1,24 @@
-let sobreMimTitulo = document.querySelector('.my-name-h1')
-let textoQuebrado = sobreMimTitulo.textContent.split('')
-sobreMimTitulo.textContent = ''
+let firstName = document.querySelector('.first-name')
+let secondName = document.querySelector('.second-name')
+let firstNameArray = firstName.textContent.split('')
+let secondNameArray = secondName.textContent.split('')
+firstName.textContent = ''
+secondName.textContent = ''
 
-for(let i = 0; i < textoQuebrado.length; i++){
-    setTimeout(function efeitoDigitalizar () {
-    console.log(sobreMimTitulo.textContent += textoQuebrado[i])
-    }, 75 * i)
+function efeitoDigitalizarSegundoNome(){
+    for (let posicaoLetraSegundoNome = 0; posicaoLetraSegundoNome < secondNameArray.length; posicaoLetraSegundoNome++) {
+        setTimeout(function() {
+            console.log(secondName.textContent += secondNameArray[posicaoLetraSegundoNome])
+        }, 150 * posicaoLetraSegundoNome)
+    }
 }
 
+for (let posicaoLetraPrimeiroNome = 0; posicaoLetraPrimeiroNome < firstNameArray.length; posicaoLetraPrimeiroNome++) {
+    setTimeout(function efeitoDigitalizarPrimeiroNome() {
+        console.log(firstName.textContent += firstNameArray[posicaoLetraPrimeiroNome])
+        if(posicaoLetraPrimeiroNome == firstNameArray.length - 1){
+            efeitoDigitalizarSegundoNome()
+        }
+    }, 150 * posicaoLetraPrimeiroNome)
+    194915
+}
---
+++ testes/teste2.js
@@ -0,0 +1,10 @@
+
+
+let arrayTeste = [1, 2 , 4, 5, 6]
+console.log(arrayTeste)
+
+
+for(let cont = 0; cont <= arrayTeste.length ; cont++){
+    console.log(cont)
+
+}
